1ad6025c88430082413bca694aa7e95e0825530c
Sandesh Patil <patilsandesh8688@gmail.com>
2026-02-17 08:59:50 +0530

    Switched to psycopg v3 driver

--- app.py
+++ app.py
@@ -15,6 +15,12 @@ app = Flask(__name__)
 database_url = os.environ.get("DATABASE_URL")
 
 if database_url:
+    # Use psycopg (v3) driver
+    if database_url.startswith("postgres://"):
+        database_url = database_url.replace("postgres://", "postgresql+psycopg://")
+    else:
+        database_url = database_url.replace("postgresql://", "postgresql+psycopg://")
+
     app.config['SQLALCHEMY_DATABASE_URI'] = database_url
 else:
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///groups.db'
